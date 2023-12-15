fda5f85968956ab298684dc61a7501e25d038aeb
radya100 <radya100@gmail.com>
2023-12-15 10:53:44 +0300

    feat: Добавил dto для вещей

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,23 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import ru.practicum.shareit.Entity;
+
+import javax.validation.constraints.NotBlank;
+
 /**
  * TODO Sprint add-controllers.
  */
-public class ItemDto {
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class ItemDto extends Entity {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String description;
+
+    private long bookingQty;
 }
