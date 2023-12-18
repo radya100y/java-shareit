a18b66b64b4fdbfcf9769032ef45b9f024682835
radya100 <radya100@gmail.com>
2023-12-18 13:33:14 +0300

    fix: Добавил логирование ошибок

--- src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.error;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,29 +8,34 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final NotFoundException exc) {
+        log.debug("Получен статус 404 Not found {}", exc.getMessage(), exc);
         return new ErrorResponse(exc.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleAlreadyExistException(final AlreadyExistException exc) {
+        log.debug("Получен статус 409 Conflict {}", exc.getMessage(), exc);
         return new ErrorResponse(exc.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadArgumentException(final MethodArgumentNotValidException exc) {
+        log.debug("Получен статус 400 Bad request {}", exc.getMessage(), exc);
         return new ErrorResponse(exc.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handleAccessException(final AccessException exc) {
+        log.debug("Получен статус 403 forbidden {}", exc.getMessage(), exc);
         return new ErrorResponse(exc.getMessage());
     }
 
