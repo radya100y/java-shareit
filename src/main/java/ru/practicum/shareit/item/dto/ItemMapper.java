a1d3bebfcb5693afebce8ef6eb440167ac8891d3
radya100 <radya100@gmail.com>
2023-12-18 13:37:26 +0300

    fix: На классах - мапперах добавил аннотацию @UtilityClass

--- src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemMapper.java
@@ -1,8 +1,10 @@
 package ru.practicum.shareit.item.dto;
 
 
+import lombok.experimental.UtilityClass;
 import ru.practicum.shareit.item.model.Item;
 
+@UtilityClass
 public class ItemMapper {
 
     public static ItemDto toItemDto(Item item) {
