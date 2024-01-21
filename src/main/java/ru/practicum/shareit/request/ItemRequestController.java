03c221d464a7520bb69533c686b1cf9753856274
radya100 <radya100@gmail.com>
2024-01-21 17:56:02 +0300

    feat: добавил RequestController

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,12 +1,34 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.dto.ItemRequestDtoIn;
+import ru.practicum.shareit.request.dto.ItemRequestDtoOut;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/requests")
 public class ItemRequestController {
+
+    @PostMapping()
+    public ItemRequestDtoOut save(@Valid @RequestBody ItemRequestDtoIn itemRequestDtoIn,
+                                  @RequestHeader("X-Sharer-User-Id") long userId) {
+        return null;
+    }
+
+    @GetMapping()
+    public List<ItemRequestDtoOut> getOwnRequests(@RequestHeader("X-Sharer-User-Id") long userId) {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ItemRequestDtoOut getRequest(@PathVariable("id") Long id, @RequestHeader("X-Sharer-User-Id") Long userId) {
+        return null;
+    }
+
+    @GetMapping("/all")
+    public List<ItemRequestDtoOut> getAllRequests(@RequestParam Long from, @RequestParam Long size) {
+        return null;
+    }
 }
