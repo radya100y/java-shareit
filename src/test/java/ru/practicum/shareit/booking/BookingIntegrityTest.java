c3426185f64f0ca8d1c2f36233ee4409dd71d612
radya100 <radya100@gmail.com>
2024-01-31 11:02:19 +0300

    fix: исправил ошибки интеграционного теста

--- src/test/java/ru/practicum/shareit/booking/BookingIntegrityTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingIntegrityTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.ItemService;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.user.User;
 import ru.practicum.shareit.user.UserService;
 import ru.practicum.shareit.user.dto.UserDto;
 
@@ -20,6 +22,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Transactional
+@Rollback(value = false)
 @SpringBootTest(properties = "db.name=test", webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class BookingIntegrityTest {
@@ -32,22 +35,25 @@ public class BookingIntegrityTest {
     @Test
     void shouldSave() {
 
-        UserDto user1 = UserDto.builder().email("qwe@qwe.qwe").name("qwe").build();
-        UserDto user2 = UserDto.builder().email("wer@wer.wer").name("wer").build();
-        ItemDto item1 = ItemDto.builder().name("qwe").description("qwe").available(true).owner(1L).build();
-        BookingDto booking = BookingDto.builder().itemId(1L)
+        UserDto owner = UserDto.builder().email("qwe@qwe.qwe").name("qwe").build();
+        UserDto savedOwner = userService.save(owner);
+
+        UserDto booker = UserDto.builder().email("wer@wer.wer").name("wer").build();
+        UserDto savedBooker = userService.save(booker);
+
+        ItemDto item1 = ItemDto.builder().name("qwe").description("qwe").available(true).owner(savedOwner.getId())
+                .build();
+        ItemDto savedItem = itemService.save(item1, savedOwner.getId());
+
+        BookingDto booking = BookingDto.builder().itemId(savedItem.getId())
                 .start(LocalDateTime.now().plus(10, ChronoUnit.HOURS))
                 .end(LocalDateTime.now().plus(20, ChronoUnit.HOURS))
-                .userId(2L).status(BookingStatus.WAITING).build();
-
-        userService.save(user1);
-        userService.save(user2);
-        itemService.save(item1, 1L);
+                .userId(savedBooker.getId()).status(BookingStatus.WAITING).build();
         bookingService.save(booking);
 
         TypedQuery<Booking> query = em.createQuery("select b from Booking b where id = :id", Booking.class);
         List<Booking> bookings = query.setParameter("id", 1L).getResultList();
 
-        Assertions.assertEquals(bookings.get(0).getBooker().getId(), 2L);
+        Assertions.assertEquals(bookings.get(0).getBooker().getId(), savedBooker.getId());
     }
 }
