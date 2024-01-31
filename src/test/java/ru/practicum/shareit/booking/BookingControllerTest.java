b8fe7b2a90729e449173336e4cefa77dc2a30102
radya100 <radya100@gmail.com>
2024-01-31 19:12:22 +0300

    fix: добавил покрытие тестов для бронирования

--- src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingMapper;
 import ru.practicum.shareit.item.model.Item;
@@ -15,13 +17,14 @@ import ru.practicum.shareit.user.User;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -72,5 +75,46 @@ public class BookingControllerTest {
                 .andExpect(jsonPath("$.status", is(BookingStatus.WAITING.name())));
     }
 
+    @Test
+    void shouldUpdate() throws Exception {
+        bookingIn.setStatus(BookingStatus.APPROVED);
+        when(service.update(anyLong(), anyLong(), any(BookingStatus.class)))
+                .thenReturn(BookingMapper.toBooking(bookingIn, item, booker));
+
+        mvc.perform(patch("/bookings/1")
+                        .param("approved", "true")
+                        .header("X-Sharer-User-Id", "1")
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", is(BookingStatus.APPROVED.name())));
+    }
+
+    @Test
+    void shouldGetForBooker() throws Exception {
+        when(service.getBookingForBooker(anyLong(), any(BookingStatusParam.class), any(Pageable.class)))
+                .thenReturn(List.of(BookingMapper.toBooking(bookingIn, item, booker)));
+
+        mvc.perform(get("/bookings")
+                        .header("X-Sharer-User-Id", "1")
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void shouldGetForOwner() throws Exception {
+        when(service.getBookingForOwner(anyLong(), any(BookingStatusParam.class), any(Pageable.class)))
+                .thenReturn(List.of(BookingMapper.toBooking(bookingIn, item, booker)));
+
+        mvc.perform(get("/bookings/owner")
+                        .header("X-Sharer-User-Id", "1")
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 
 }
--- src/test/java/ru/practicum/shareit/booking/BookingServiceTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingServiceTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.error.AccessException;
 import ru.practicum.shareit.error.NotFoundException;
+import ru.practicum.shareit.error.ValidateException;
 import ru.practicum.shareit.item.ItemRepository;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
@@ -16,6 +19,8 @@ import ru.practicum.shareit.user.UserRepository;
 import ru.practicum.shareit.user.dto.UserDto;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -32,12 +37,11 @@ public class BookingServiceTest {
 
     private BookingService bookingService;
 
-    private final UserDto userDto = UserDto.builder().id(1L).email("qwe@qwe.qwe").name("qwe").build();
     private final User user = User.builder().id(1L).email("qwe@qwe.qwe").name("qwe").build();
-    private final ItemDto itemDto = ItemDto.builder().id(1L).name("wer").description("wer").available(false).build();
     private final Item item = Item.builder().id(1L).name("qwe").description("qwe").available(true).owner(1L).build();
-    private final BookingDto bookingDto = BookingDto.builder().id(1L).itemId(1L).start(LocalDateTime.now())
-            .end(LocalDateTime.now()).build();
+    private final BookingDto bookingDto = BookingDto.builder().id(1L).itemId(1L)
+            .start(LocalDateTime.now().plus(10, ChronoUnit.HOURS))
+            .end(LocalDateTime.now().plus(100, ChronoUnit.HOURS)).build();
     private final Booking booking = Booking.builder().id(1).item(item).start(LocalDateTime.now())
             .end(LocalDateTime.now()).status(BookingStatus.WAITING).booker(user).build();
 
@@ -53,35 +57,121 @@ public class BookingServiceTest {
     @Test
     @DisplayName("Запись бронирования")
     void shouldSave() {
-            when(userRepository.findById(anyLong())).thenReturn(Optional.ofNullable(user));
-            when(itemRepository.findById(anyLong())).thenReturn(Optional.ofNullable(item));
-            when(bookingRepository.save(any(Booking.class))).thenReturn(booking);
+        when(userRepository.findById(anyLong())).thenReturn(Optional.ofNullable(user));
+        when(itemRepository.findById(anyLong())).thenReturn(Optional.ofNullable(item));
+        when(bookingRepository.save(any(Booking.class))).thenReturn(booking);
+        Assertions.assertEquals(bookingService.save(bookingDto).getBooker().getId(), 1);
 
-            bookingDto.setStart(LocalDateTime.parse("2024-02-02T00:00:00"));
-            bookingDto.setEnd(LocalDateTime.parse("2024-03-03T00:00:00"));
+        bookingDto.setStart(null);
+        Assertions.assertThrows(ValidateException.class, () -> bookingService.save(bookingDto));
 
-            Assertions.assertEquals(bookingService.save(bookingDto).getBooker().getId(), 1);
+        bookingDto.setStart(LocalDateTime.now().plus(1000, ChronoUnit.HOURS));
+        Assertions.assertThrows(ValidateException.class, () -> bookingService.save(bookingDto));
+
+        assert item != null;
+        item.setAvailable(false);
+        Assertions.assertThrows(ValidateException.class, () -> bookingService.save(bookingDto));
     }
 
     @Test
     @DisplayName("Обновление бронирования")
     void shouldUpdate() {
+        when(bookingRepository.findById(1L)).thenReturn(Optional.ofNullable(booking));
+        when(itemRepository.findById(anyLong())).thenReturn(Optional.ofNullable(item));
+        when(userRepository.findById(anyLong())).thenReturn(Optional.ofNullable(user));
         when(bookingRepository.save(any(Booking.class))).thenReturn(booking);
 
-        bookingDto.setStart(LocalDateTime.parse("2024-02-02T00:00:00"));
-        bookingDto.setEnd(LocalDateTime.parse("2024-03-03T00:00:00"));
-        bookingDto.setUserId(2L);
+        Assertions.assertEquals(bookingService.update(1L, 1L, BookingStatus.REJECTED)
+                .getId(), 1L);
+        Assertions.assertThrows(ValidateException.class, () ->
+                bookingService.update(1L, 1L, BookingStatus.REJECTED));
 
-        Assertions.assertThrows(NotFoundException.class, () -> bookingService.save(bookingDto));
     }
 
     @Test
-    @DisplayName("ПРосмотр бронирования")
+    @DisplayName("Просмотр бронирования")
     void shouldGet() {
         when(bookingRepository.findById(anyLong())).thenReturn(Optional.ofNullable(booking));
-
-        bookingDto.setStart(LocalDateTime.parse("2024-02-02T00:00:00"));
-        bookingDto.setEnd(LocalDateTime.parse("2024-03-03T00:00:00"));
         Assertions.assertEquals(bookingService.getCertain(1L, 1L).getStatus(), BookingStatus.WAITING);
+
+        Assertions.assertThrows(AccessException.class,
+                () -> bookingService.getCertain(1L, 3L).getStatus());
     }
+
+    @Test
+    @DisplayName("Просмотр бронирований")
+    void shouldAny() {
+        when(bookingRepository.findByItem_IdAndStartIsBeforeOrderByEndDesc(anyLong(), any(LocalDateTime.class)))
+                .thenReturn(List.of(booking));
+        when(bookingRepository.findAllByItem_IdAndBooker_IdAndStatusAndEndIsBefore(anyLong(), anyLong(),
+                any(BookingStatus.class), any(LocalDateTime.class))).thenReturn(List.of(booking));
+
+
+        Assertions.assertNotEquals(bookingService.getBookingByItemOld(1L), 0);
+        Assertions.assertNotEquals(bookingService.getBookingByItemNew(1L, BookingStatus.WAITING), 0);
+        Assertions.assertNotEquals(bookingService.getBookingByItemUserStatusOld(1L, 1L), 0);
+    }
+
+    @Test
+    @DisplayName("Получаем бронирования по заявителю")
+    void shouldGetByBooker() {
+        when(userRepository.findById(anyLong())).thenReturn(Optional.ofNullable(user));
+        when(bookingRepository.findAllByBooker_Id(anyLong(), any(Pageable.class))).thenReturn(List.of(booking));
+        when(bookingRepository.findAllByBooker_IdAndStartIsBeforeAndEndIsAfterOrderByIdAsc(anyLong(),
+                any(LocalDateTime.class), any(LocalDateTime.class))).thenReturn(List.of(booking));
+        when(bookingRepository.findAllByBooker_IdAndStatusAndEndIsBeforeOrderByIdDesc(anyLong(),
+                any(BookingStatus.class), any(LocalDateTime.class))).thenReturn(List.of(booking));
+        when(bookingRepository.findAllByBooker_IdAndStatusInAndStartIsAfterOrderByIdDesc(anyLong(), any(),
+                any(LocalDateTime.class))).thenReturn(List.of(booking));
+        when(bookingRepository.findAllByBooker_IdAndStatusOrderByIdDesc(anyLong(), any(BookingStatus.class)))
+                .thenReturn(List.of(booking));
+        when(bookingRepository.findAllByBooker_IdAndStatusOrderByIdDesc(anyLong(), any(BookingStatus.class)))
+                .thenReturn(List.of(booking));
+
+        Assertions.assertEquals(bookingService.getBookingForBooker(1L,
+                BookingStatusParam.ALL, Pageable.ofSize(10)).size(), 1);
+        Assertions.assertEquals(bookingService.getBookingForBooker(1L,
+                BookingStatusParam.CURRENT, Pageable.ofSize(10)).size(), 1);
+        Assertions.assertEquals(bookingService.getBookingForBooker(1L,
+                BookingStatusParam.PAST, Pageable.ofSize(10)).size(), 1);
+        Assertions.assertEquals(bookingService.getBookingForBooker(1L,
+                BookingStatusParam.FUTURE, Pageable.ofSize(10)).size(), 1);
+        Assertions.assertEquals(bookingService.getBookingForBooker(1L,
+                BookingStatusParam.WAITING, Pageable.ofSize(10)).size(), 1);
+        Assertions.assertEquals(bookingService.getBookingForBooker(1L,
+                BookingStatusParam.REJECTED, Pageable.ofSize(10)).size(), 1);
+    }
+
+    @Test
+    @DisplayName("Получение бронирований по владельцу")
+    void shouldGetByOwner() {
+        when(userRepository.findById(anyLong())).thenReturn(Optional.ofNullable(user));
+        when(bookingRepository.findAllByItemOwner(anyLong(), any(Pageable.class))).thenReturn(List.of(booking));
+        when(bookingRepository.findAllByItemOwnerAndStartIsBeforeAndEndIsAfterOrderByIdDesc(anyLong(),
+                any(LocalDateTime.class), any(LocalDateTime.class))).thenReturn(List.of(booking));
+        when(bookingRepository.findAllByItemOwnerAndStatusAndEndIsBeforeOrderByIdDesc(anyLong(),
+                any(BookingStatus.class), any(LocalDateTime.class))).thenReturn(List.of(booking));
+        when(bookingRepository.findAllByItemOwnerAndStatusInAndStartIsAfterOrderByIdDesc(
+                anyLong(), any(), any(LocalDateTime.class))).thenReturn(List.of(booking));
+        when(bookingRepository.findAllByItemOwnerAndStatusOrderByIdDesc(anyLong(), any(BookingStatus.class)))
+                .thenReturn(List.of(booking));
+        when(bookingRepository.findAllByItemOwnerAndStatusOrderByIdDesc(anyLong(), any(BookingStatus.class)))
+                .thenReturn(List.of(booking));
+
+        Assertions.assertEquals(bookingService.getBookingForOwner(1L, BookingStatusParam.ALL,
+                Pageable.ofSize(10)).size(), 1);
+        Assertions.assertEquals(bookingService.getBookingForOwner(1L, BookingStatusParam.CURRENT,
+                Pageable.ofSize(10)).size(), 1);
+        Assertions.assertEquals(bookingService.getBookingForOwner(1L, BookingStatusParam.PAST,
+                Pageable.ofSize(10)).size(), 1);
+        Assertions.assertEquals(bookingService.getBookingForOwner(1L, BookingStatusParam.FUTURE,
+                Pageable.ofSize(10)).size(), 1);
+        Assertions.assertEquals(bookingService.getBookingForOwner(1L, BookingStatusParam.WAITING,
+                Pageable.ofSize(10)).size(), 1);
+        Assertions.assertEquals(bookingService.getBookingForOwner(1L, BookingStatusParam.REJECTED,
+                Pageable.ofSize(10)).size(), 1);
+
+    }
+
+
 }
