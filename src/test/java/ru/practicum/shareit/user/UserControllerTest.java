0d1987fef1ecffb7ff95f48fed69dbfb3c5fc0d3
radya100 <radya100@gmail.com>
2024-02-02 09:34:40 +0300

    fix: чекстайл

--- src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import ru.practicum.shareit.error.AccessException;
 import ru.practicum.shareit.error.AlreadyExistException;
 import ru.practicum.shareit.error.ValidateException;
