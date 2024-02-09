b1d8a37eaa02b94cb2c50010ac3359fcd3f540d7
radya100 <radya100@gmail.com>
2024-02-09 13:28:50 +0300

    feat: Добавил контроллер на юзеров. Понятно куда пишется

--- gateway/src/main/java/ru/practicum/shareit/user/dto/UserIn.java
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserIn.java
@@ -1,9 +1,16 @@
 package ru.practicum.shareit.user.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+@Getter
+@Setter
+@ToString
 public class UserIn {
 
     private Long id;
